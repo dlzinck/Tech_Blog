2ed1d6bca32f807bc4fb92ed019c074ab1c32d3b
Drew Zinck <dlzincktheii@gmail.com>
2022-04-24 14:33:06 -0700

    Added models

---
+++ models/Post.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+// CREATE POST MODEL
+class Post extends Model {}
+
+// CREATE FIELDS & COLUMNS FOR POST MODEL
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        postTitle: {
+            type: DataTypes.STRING(25),
+            allowNull: false,
+            unique: true,
+        },
+        postContent: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        dateCreated: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },  
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
